4adaf4d273d84f8e837c98ed20ec22349be5c1cf
sharmadotexe <rockvikas300@gmail.com>
2024-02-25 12:13:25 +0530

    basic setup for application

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const const express = require('express')
+const  express = require('express')
 const http=require('http')
-
-
-
-
 const app = express()
+const path = require('path')
 const server = http.createServer(app)
 
+app.use(express.static(path.resolve("./public")))
+
+
+app.get('/', (req, res) => {
+    res.sendFile("./public/index.html");
+});
+
+ 
 server.listen(9000, ()=> console.log('server starter at 9000 port')  )
 
 
